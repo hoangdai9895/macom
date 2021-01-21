faccf9d9240bf4c0744585aacdd53e6c6c474f8a
phucpapai <phuc.phung@vmodev.com>
2021-01-21 09:15:37 +0700

    change

--- assets/js/wwd.js
+++ assets/js/wwd.js
@@ -1,10 +1,10 @@
 let flag = false;
 const HEIGHT = window.innerHeight;
-const BLACK_SRC = "/assets/img/menu_icon.png";
-const WHITE_SRC = "/assets/img/menu_icon_white.png";
+const BLACK_SRC = "./assets/img/menu_icon.png";
+const WHITE_SRC = "./assets/img/menu_icon_white.png";
 
-const WHITE_SRC_LG = "/assets/img/Type-white.png";
-const BLACK_SRC_LG = "/assets/img/Type.png";
+const WHITE_SRC_LG = "./assets/img/Type-white.png";
+const BLACK_SRC_LG = "./assets/img/Type.png";
 
 
 
